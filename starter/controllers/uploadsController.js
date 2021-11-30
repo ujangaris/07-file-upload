34a1ceeaab9aaa319d8230891136313221d3194c
ujangaja <ujangaja@gmail.com>
2021-11-30 16:33:34 +0700

    #Section 10: File/Image Upload | 217. EXPRESS-FILEUPLOAD Package

--- starter/controllers/uploadsController.js
+++ starter/controllers/uploadsController.js
@@ -1,8 +1,18 @@
+const path = require('path');
 const { StatusCodes } = require('http-status-codes');
 
 const uploadProductImage = async (req, res) => {
-  console.log(req);
-  res.send('upload product Image');
+  // console.log(req.files);
+  const productImage = req.files.image;
+  const imagePath = path.join(
+    __dirname,
+    '../public/uploads/' + `${productImage.name}`
+  );
+  await productImage.mv(imagePath);
+
+  return res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${productImage.name}` } });
 };
 
 module.exports = {
