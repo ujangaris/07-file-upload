750bd86d1e276f3a1c667a71bf93c6b048163121
ujangaja <ujangaja@gmail.com>
2021-11-30 17:18:10 +0700

    #Section 10: File/Image Upload | 219. Additional Checks

--- starter/controllers/uploadsController.js
+++ starter/controllers/uploadsController.js
@@ -1,9 +1,26 @@
 const path = require('path');
 const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
 
 const uploadProductImage = async (req, res) => {
   // console.log(req.files);
+  // check if file exists
+  if (!req.files) {
+    throw new CustomError.BadRequestError('No File Uploaded');
+  }
   const productImage = req.files.image;
+
+  // check format
+  if (!productImage.mimetype.startsWith('image')) {
+    throw new CustomError.BadRequestError('Please Upload Image');
+  }
+  // check size
+  // const maxSize = 1000;//ini untuk test kodisi dibawah , tentunya gambar besar tidak akan lolos
+  const maxSize = 1024 * 1024;
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError('Please Upload Image smaller 1KB');
+  }
+
   const imagePath = path.join(
     __dirname,
     '../public/uploads/' + `${productImage.name}`
